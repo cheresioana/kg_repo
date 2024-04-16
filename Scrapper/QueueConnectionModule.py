e150d73ba25799fc39b2b3fcf45a97fc61a186ec
mtopor <mike.topor@yahoo.com>
2024-04-16 09:47:38 +0300

    queue changes

--- Scrapper/QueueConnectionModule.py
+++ Scrapper/QueueConnectionModule.py
@@ -1,3 +1,5 @@
+import time
+
 import pika
 
 '''
@@ -7,12 +9,28 @@ The class sends data to RabbitMQ, from where it is read by the Aggregator compon
 
 class QueueConnectionModule:
     def __init__(self):
-        self.connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+        self.connection_params = pika.ConnectionParameters('localhost')
+        self.queue_name = 'crawlers'
+        self.connect()
+
+    def connect(self):
+        """Metoda pentru a stabili conexiunea și canalul."""
+        print("Connect Rabbit MQ")
+        self.connection = pika.BlockingConnection(self.connection_params)
         self.channel = self.connection.channel()
-        self.channel.queue_declare(queue='crawlers')
+        self.channel.queue_declare(queue=self.queue_name)
 
     def send_message(self, message):
-        self.channel.basic_publish(exchange='',
-                                   routing_key='crawlers',
-                                   body=message)
-        print(" [crawler] Sent " + message[:200])
+        try:
+            self.channel.basic_publish(exchange='',
+                                       routing_key='crawlers',
+                                       body=message)
+            print(" [crawler] Sent " + message[:200])
+        except (pika.exceptions.StreamLostError, pika.exceptions.ChannelClosedByBroker) as e:
+            print("Eroare la trimiterea mesajului, încerc reconectarea: ", e)
+            print("Reconectare la RabbitMQ...")
+            self.connect()
+            self.channel.basic_publish(exchange='',
+                                       routing_key='crawlers',
+                                       body=message)
+            print(" [crawler] Sent after reconnect " + message[:200])
